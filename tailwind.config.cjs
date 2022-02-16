e35372e95145e6ece97559d57485bbdd9f48b46b
rickvanbalkom <rickvanbalkom@users.noreply.github.com>
2022-02-16 21:11:32 +0000

    added some pages and content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,4 +3,9 @@ module.exports = {
       './public/**/*.html',
       './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
     ],
+    theme: {
+      fontFamily: {
+        'sans': ['Calibri', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Robot', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
+      }
+    }
   };

87d36d4bf7747a937de9377f7ff86a4b58b83401
rickvanbalkom <rickvanbalkom@users.noreply.github.com>
2022-02-26 14:38:51 +0000

    start for project pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,11 @@ module.exports = {
       './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
     ],
     theme: {
+      extend: {
+        gridTemplateColumns: {
+          'project': '36.4% 13.6% 50%'
+        }
+      },
       fontFamily: {
         'sans': ['Calibri', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Robot', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
       }
